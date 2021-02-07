05272b92fdfe3e24b1207e57fefad75acda62d79
Jason Thompson <crytilis@gmail.com>
2021-02-07 00:33:42 -0600

    Add project files.

---
+++ CircularProgressBar_NET5_Sample/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CircularProgressBar_NET5_Sample
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CircularProgressBar_NET5_Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CircularProgressBar_NET5_Sample
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
